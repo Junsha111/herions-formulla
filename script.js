d7bafc2f964dca42fbfdc51c00bd6f2b6ed0ce5c
Junsha111 <shahjunaid999@gmail.com>
2024-05-20 19:44:19 +0530

    area of a triangle using herions formula

---
+++ script.js
@@ -0,0 +1,73 @@
+// let username = prompt("Enter user name")
+// let pasword =  prompt("Enter password")
+
+
+// if(username =="junaid shah" && pasword == "123456"){
+//     console.log("login")
+// }else{
+//     console.log("wronge Credential")
+// }
+// console.log(randNum); 
+// while(randNum <50){
+//     console.log("hello")
+// }
+// do{
+//     console.log(randNum)
+// }
+// while(true){
+
+// }
+// var randNum = Number.parseInt(Math.random()*100);
+// do{
+//     var guess = Number.parseInt(prompt('Guess the number:'));
+//     if(guess == randNum){
+//         ("You win!");
+//         break;
+//     }
+//     else if (guess>randNum){
+//         console.log("your number is greater")
+//     }
+//     else if ( guess < randNum){
+//         console.log("Your number is less")
+//     }
+//     }
+// while(true)
+    // for(var i=0; i<=15; i++){
+    //     if(i%2==0){
+    //         console.log(`${i} is even`)
+    //     }
+    //     else{
+    //         console.log(`${i} is odd`)
+    //     }
+    // }
+// function add(){
+//     var x = 20 ;
+//     var y =  30 ;
+//     console.log(x + y);
+// }
+// add();
+// function add(x , y){
+//     return x + y ;
+// }
+// console.log(add(5 , 6));
+// const sub = (x , y)=>{
+//     return x - y
+// }
+// let name = "54533";
+// function perimeter(a,b,c) {
+//     return (a+b+c)
+    
+// }
+// perimeter(10,5,6)
+// console.log(perimeter(10,5,6))
+function her (a,b,c){
+    var s=(a+b+c)/2
+    var x =s-a
+    var y=s-b
+    var z=s-c;
+    var form = Math.sqrt(s*x*y*z)
+    return form
+
+}
+var result=her (4,5,6)
+console.log(result)
